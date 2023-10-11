a7930708c1b7d1a1c575ad174e48bf251918e47f
alabj <alamuhammed005@gmail.com>
2023-10-11 12:13:44 +0100

    third coomit

--- src/startUp/CreateAccount.jsx
+++ src/startUp/CreateAccount.jsx
@@ -1,5 +1,5 @@
-import { useForm } from "../formReducer/Form";
-import React, { useContext } from "react";
+// import { useForm } from "../formReducer/Form";
+// import React, { useContext } from "react";
 import { useNavigate } from 'react-router-dom';
 import cuate from '../assets/ForStartUp/cuate.svg'
 import Google from '../assets/ForStartUp/Google.png'
@@ -7,103 +7,103 @@ import Facebook from '../assets/ForStartUp/Facebook.png'
 import Apple from '../assets/ForStartUp/Apple.png'
 
 const CreateAccount = () => {
-    const { state, dispatch } = useForm();
+    // const { state, dispatch } = useForm();
 
-    const info = {
-      surname: state.surname,
-      email: state.EmailAddress,
-      password: state.Password,
-      othernames: state.OtherName,
-      gender: state.Gender,
-      repeat_password: state.RepeatPassword,
-      phone: state.Phone,
-    };
+    // const info = {
+    //   surname: state.surname,
+    //   email: state.EmailAddress,
+    //   password: state.Password,
+    //   othernames: state.OtherName,
+    //   gender: state.Gender,
+    //   repeat_password: state.RepeatPassword,
+    //   phone: state.Phone,
+    // };
   
-    function handleSubmit(e) {
-      e.preventDefault();
-      if (
-        !info.surname ||
-        !info.email ||
-        !info.phone ||
-        !info.password ||
-        !info.repeat_password ||
-        !info.gender ||
-        !info.othernames
-      ) {
-        toast.error("Please fill in all fields");
-        return;
-      }
+    // function handleSubmit(e) {
+    //   e.preventDefault();
+    //   if (
+    //     !info.surname ||
+    //     !info.email ||
+    //     !info.phone ||
+    //     !info.password ||
+    //     !info.repeat_password ||
+    //     !info.gender ||
+    //     !info.othernames
+    //   ) {
+    //     toast.error("Please fill in all fields");
+    //     return;
+    //   }
   
-      dispatch({ type: "START_FETCH" });
-      fetch("https://carbon-api-test.azurewebsites.net/api/v1/user/register", {
-        method: "POST",
-        body: JSON.stringify(info),
-        headers: {
-          "Content-type": "application/json; charset=UTF-8",
-        },
-      })
-        .then((response) => response.json())
-        .then((json) => {
-          console.log(json);
-          dispatch({ type: "DATA_FETCHED", payload: json });
-          toast.success("sign up successful");
-        })
+    //   dispatch({ type: "START_FETCH" });
+    //   fetch("https://carbon-api-test.azurewebsites.net/api/v1/user/register", {
+    //     method: "POST",
+    //     body: JSON.stringify(info),
+    //     headers: {
+    //       "Content-type": "application/json; charset=UTF-8",
+    //     },
+    //   })
+    //     .then((response) => response.json())
+    //     .then((json) => {
+    //       console.log(json);
+    //       dispatch({ type: "DATA_FETCHED", payload: json });
+    //       toast.success("sign up successful");
+    //     })
   
-        // dispatch({type: "RESET"})
+    //     // dispatch({type: "RESET"})
   
-        .catch((error) => {
-          console.log(error);
-          dispatch({ type: "ERROR" });
-          toast.error("please fill in all fields");
-        });
-    }
+    //     .catch((error) => {
+    //       console.log(error);
+    //       dispatch({ type: "ERROR" });
+    //       toast.error("please fill in all fields");
+    //     });
+    // }
   
-    function getSurname(e) {
-      dispatch({
-        type: "DATA_FETCHED",
-        field: "surname",
-        payload: e.target.value,
-      });
-    }
+    // function getSurname(e) {
+    //   dispatch({
+    //     type: "DATA_FETCHED",
+    //     field: "surname",
+    //     payload: e.target.value,
+    //   });
+    // }
   
-    function getEmail(e) {
-      dispatch({
-        type: "DATA_FETCHED",
-        field: "EmailAddress",
-        payload: e.target.value,
-      });
-    }
+    // function getEmail(e) {
+    //   dispatch({
+    //     type: "DATA_FETCHED",
+    //     field: "EmailAddress",
+    //     payload: e.target.value,
+    //   });
+    // }
   
-    function getOtherName(e) {
-      dispatch({
-        type: "DATA_FETCHED",
-        field: "OtherName",
-        payload: e.target.value,
-      });
-    }
+    // function getOtherName(e) {
+    //   dispatch({
+    //     type: "DATA_FETCHED",
+    //     field: "OtherName",
+    //     payload: e.target.value,
+    //   });
+    // }
   
-    function getPassword(e) {
-      dispatch({ type: "DATA_FETCHED", field: "Password", payload: e.target.value });
-    }
-    function getRepeatPassword(e) {
-      dispatch({
-        type: "DATA_FETCHED",
-        field: "RepeatPassword",
-        payload: e.target.value,
-      });
-    }
+    // function getPassword(e) {
+    //   dispatch({ type: "DATA_FETCHED", field: "Password", payload: e.target.value });
+    // }
+    // function getRepeatPassword(e) {
+    //   dispatch({
+    //     type: "DATA_FETCHED",
+    //     field: "RepeatPassword",
+    //     payload: e.target.value,
+    //   });
+    // }
 
-    function getPhone(e) {
-        dispatch({ type: "DATA_FETCHED", field: "Phone", payload: e.target.value });
-      }
+    // function getPhone(e) {
+    //     dispatch({ type: "DATA_FETCHED", field: "Phone", payload: e.target.value });
+    //   }
 
-    function getGender(e) {
-      dispatch({
-        type: "DATA_FETCHED",
-        field: "Gender",
-        payload: e.target.value,
-      });
-    }
+    // function getGender(e) {
+    //   dispatch({
+    //     type: "DATA_FETCHED",
+    //     field: "Gender",
+    //     payload: e.target.value,
+    //   });
+    // }
 
     const navigate = useNavigate();
 
@@ -115,8 +115,8 @@ const CreateAccount = () => {
     }
 
   return (
-    <form className='rounded-md max-w-[1000px] my-[30px] mx-auto bg-[#ffffff]' onSubmit={handleSubmit}>
-        <div className=" border flex rounded-md sm:flex-wrap sm:w-[550px] sm:px-7">
+    <form className='rounded-md max-w-[1000px] my-[30px] mx-auto bg-[#ffffff] ' >
+        <div className=" border flex rounded-md sm:flex-wrap sm:w-[550px] sm:px-5 sm:border-none">
             <div className="bg-[#F8E5C2] w-[30%] sm:w-full sm:h-[15rem]">
                 <div className="">
                     <img src={cuate} alt="" className="w-[20rem] my-[10rem] sm:w-[7rem] sm:my-[5rem] sm:mx-auto" />
@@ -137,14 +137,14 @@ const CreateAccount = () => {
                         <label htmlFor="Amount" className='font-semibold mr-[13rem] sm:mr-[21.9rem]'>First Name</label>
                         <input type="text" id='Amount' className='border
                         text-gray-900 text-sm rounded block p-4 w-[15rem] sm:w-[27rem]' placeholder='John'
-                        onChange={getOtherName}/>
+                       />
                         </div>
 
                     <div className="sm:pt-9">
                         <label htmlFor="Amount" className='font-semibold mr-[13rem] sm:mr-[21.9rem]'>Last Name</label>
                         <input type="text" id='Amount' className='border
                         text-gray-900 text-sm rounded block p-4 w-[15rem] sm:w-[27rem]' placeholder='Doe'
-                        onChange={getSurname}/>
+                     />
                     </div>
                     </div>
 
@@ -153,14 +153,14 @@ const CreateAccount = () => {
                     <label htmlFor="Amount" className='font-semibold mr-[13rem]  sm:mr-[24.9rem]'>Email</label>
                     <input type="text" id='Amount' className='border border-solid
                      text-gray-900 text-sm rounded block p-4 w-[15rem] sm:w-[27rem]' placeholder='example@email.com'
-                     onChange={getEmail}/>
+                    />
                      </div>
 
                     <div className="ml-[2rem] sm:pt-9 sm:ml-[2px]">
                     <label htmlFor="Amount" className='font-semibold mr-[13rem]  sm:mr-[23.9rem]'>Number</label>
                     <input type="text" id='Amount' className='border
                      text-gray-900 text-sm rounded block p-4 w-[15rem] sm:w-[27rem]' placeholder='09035672438'
-                     onChange={getPhone}/>
+                     />
                     </div>
                     </div>
                     
@@ -169,14 +169,14 @@ const CreateAccount = () => {
                         <label htmlFor="Amount" className='font-semibold mr-[13rem] sm:mr-[21.9rem]'>Password</label>
                         <input type="password" id='Amount' className='border border-solid
                         text-gray-900 text-sm rounded block p-4 w-[15rem] sm:w-[27rem]' placeholder='example@email.com'
-                        onChange={getPassword}/>
+                        />
                         </div>
 
                         <div className="sm:pt-9">
                         <label htmlFor="Amount" className='font-semibold mr-[13rem] sm:mr-[19.9rem]'>Confirm Password</label>
                         <input type="password" id='Amount' className='border
                         text-gray-900 text-sm rounded block p-4 w-[15rem] sm:w-[27rem]' placeholder='koai87%*$#@'
-                        onChange={getRepeatPassword}/>
+                      />
                         </div>
                     </div>
                 </div>
